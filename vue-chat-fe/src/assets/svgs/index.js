b31597eed8732f9bf00c216b5acfaac4eb82eb17
zhoulin xie <zhoulin.xie@daocloud.io>
2019-06-19 22:52:42 +0800

    add routers

---
+++ vue-chat-fe/src/assets/svgs/index.js
@@ -0,0 +1,7 @@
+import './icon_male.svg';
+import './icon_female.svg';
+import './icon_people.svg';
+import './icon_search.svg';
+import './icon_message.svg';
+import './icon_add.svg';
+import './icon_delete.svg';
--- vue-chat-fe/src/router.js
+++ vue-chat-fe/src/router.js
@@ -1,23 +1,61 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import store from './store';
+import Home from './views/home/home.vue';
+import Login from './views/login/login.vue';
+import NotFound from './views/not-found/not-found.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
+const router = new Router({
+  linkActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) { // eslint-disable-line
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { x: 0, y: 0 };
+  },
+
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        public: true,
+      },
+    },
+    {
+      path: '*',
+      component: NotFound,
+    },
+  ],
+});
+
+// 注册全局钩子用来拦截导航
+router.beforeEach((to, from, next) => { // eslint-disable-line
+  // 获取 store 里面的 token
+  const { token } = store.state;
+  // 判断要去的路由有没有requiresAuth
+  if (to.meta.requiresAuth) {
+    if (token) {
+      next();
+    } else {
+      next({
+        name: 'login',
+      });
     }
-  ]
-})
+  } else {
+    next();
+  }
+});
+
+export default router;
